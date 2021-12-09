15c3d67208421cb4d55ca0323a6c99b3cb100ea7
advaitvd <advaitvdgoa@gmail.com>
2021-12-09 15:29:29 +0530

    population init, random solution generation

--- MDBA.py
+++ MDBA.py
@@ -3,11 +3,28 @@ import pandas as pd
 import numpy as np
 
 
-class GenerateSol:
-    def __init__(self,x,ma):
-        K=ma.assembleStiffness(x)
-        self.w,self.v=ma.solve_eig(K,ma.M)
-        
+def generate_solution(n):
+    '''
+    Generates a random solution for size n
+    parameters:
+        n - number of elements
+    returns: randomly generated damage parameters vector of size n
+    '''
+    return 1-np.random.random(size=n)
+
+def initialize_population(pop_size,n):
+    '''
+    Initializes population with size of pop_size and n number of elements.
+    parameters: 
+        pop_size - integer, population size
+        n - integer, number of elements
+    returns: list of numpy arrays of size pop_size where each array is of size n
+    '''
+    pop=[]
+    for i in range(pop_size):
+        pop.append(generate_solution(n))
+    
+    return pop
 
 class MDBA:
     def __init__(self):
@@ -15,15 +32,17 @@ class MDBA:
 
 
 if __name__=="__main__":
-    file_name = '2D-data.xlsx'
-    dimension = int(file_name[0])
-    elements = pd.read_excel(file_name, sheet_name='Sheet1').values[:, 1:]
-    nodes = pd.read_excel(file_name, sheet_name='Sheet2').values[:, 1:]
-    aa = ma.ModalAnalysis(elements, nodes, dimension)
+    # file_name = '2D-data.xlsx'
+    # dimension = int(file_name[0])
+    # elements = pd.read_excel(file_name, sheet_name='Sheet1').values[:, 1:]
+    # nodes = pd.read_excel(file_name, sheet_name='Sheet2').values[:, 1:]
+    # aa = ma.ModalAnalysis(elements, nodes, dimension)
 
-    M = aa.assembleMass()
-    K = aa.assembleStiffness(np.ones(elements.shape[0]))
+    # M = aa.assembleMass()
+    # K = aa.assembleStiffness(np.ones(elements.shape[0]))
 
-    w, v = aa.solve_eig(K, M)
-    print(w.shape, v.shape)
-    print(w)
+    # w, v = aa.solve_eig(K, M)
+    # print(w.shape, v.shape)
+    # print(w)
+
+    print(initialize_population(10,5))
