7ff958806a411be2b70b3052046005b96b72e743
advaitvd <advaitvdgoa@gmail.com>
2021-12-11 21:11:30 +0530

    bat algorithm basic working code

--- MDBA.py
+++ MDBA.py
@@ -3,32 +3,98 @@ import pandas as pd
 import numpy as np
 
 
-def generate_solution(n):
-    '''
-    Generates a random solution for size n
-    parameters:
-        n - number of elements
-    returns: randomly generated damage parameters vector of size n
-    '''
-    return 1-np.random.random(size=n)
-
-def initialize_population(pop_size,n):
-    '''
-    Initializes population with size of pop_size and n number of elements.
-    parameters: 
-        pop_size - integer, population size
-        n - integer, number of elements
-    returns: list of numpy arrays of size pop_size where each array is of size n
-    '''
-    pop=[]
-    for i in range(pop_size):
-        pop.append(generate_solution(n))
-    
-    return pop
-
 class MDBA:
-    def __init__(self):
-        pass
+    def __init__(self, n_vars,cost_func, population_size=10, Ub=1, Lb=0, num_iterations=100):
+        self.population_size=population_size
+        self.cost_func = cost_func
+
+        self.options={
+                    'ub':Ub,
+                    'lb':Lb,
+                    'n_vars':n_vars,
+                    'population_size':population_size
+                    }
+
+        self.population, self.fitness = self.initialize_population()
+
+        
+        self.loudness=np.ones(shape=(self.population_size,1),dtype=np.float32)
+        self.pulse_rate=np.zeros(shape=(self.population_size,1),dtype=np.float32)
+        self.frequency=np.zeros(shape=(self.population_size,1),dtype=np.float32)
+        self.velocity=np.zeros(shape=(self.population_size, n_vars),dtype=np.float32)
+
+        # algo parameters
+        self.num_iterations = num_iterations
+        self.frequency_range=np.array([0,1], dtype=np.float32)
+        self.loudness_decay = 0.9
+        self.loudness_lim = 0.05
+        self.pulse_rate_decay = 0.5
+        self.gamma = 0.5
+
+        #results
+        self.best_position = np.empty(shape=(1,n_vars),dtype=np.float32)
+        self.best_fitness = 0.0
+        self.best_bat = 0
+
+    def initialize_population(self):
+        population = np.empty(shape=(self.population_size,self.options['n_vars']),dtype=np.float)
+        fitness = np.empty(shape=(self.population_size,1),dtype=np.float)
+
+        population = self.options['lb']+(self.options['ub']-self.options['lb'])*np.random.random(size=(self.population_size,self.options['n_vars']))
+        for i in range(self.population_size):
+            fitness[i,0]=self.cost_func(population[i,:])
+        
+        return population, fitness
+    
+    def update_bat(self, bat, best):
+        self.frequency[bat,:]=self.frequency_range[0]+(self.frequency_range[1]-self.frequency_range[0])*np.random.random()
+        self.velocity[bat,:]=(self.population[best,:]-self.population[bat,:])*self.frequency[bat,:]
+        position=self.population[bat,:]+self.velocity[bat,:]
+
+        return position
+
+    def run(self):
+        best_fit=self.fitness.min()
+        best_index=self.fitness.argmin()
+
+        #iteration
+        for step in range(1,self.num_iterations +1):
+            print(best_fit)
+            for bat in range(self.population_size):
+                
+                #update bat variables
+                position = self.update_bat(bat,best_index)
+
+                #random walk
+                if np.random.random()<self.loudness[bat,:]:
+                    position+=self.loudness.mean()*np.random.uniform(-1,1,size=(self.options['n_vars'],))
+                
+                #check limits
+                position[position>self.options['ub']]=self.options['ub']
+                position[position<self.options['lb']]=self.options['lb']
+
+                #check bat fitness
+                bat_fitness = self.cost_func(position)
+
+                if bat_fitness < self.fitness[bat,:]:
+                    self.fitness[bat,:]=bat_fitness
+                    self.population[bat,:]=position
+                    self.pulse_rate[bat,:]=self.pulse_rate_decay*(1-np.exp(-self.gamma*step))
+
+                    if self.loudness[bat,0]>self.loudness_lim:
+                        self.loudness[bat,0]*=self.loudness_decay
+                    else:
+                        self.loudness[bat,0]=self.loudness_lim
+                    
+                    if bat_fitness < best_fit:
+                        best_fit = bat_fitness
+                        best_index = bat
+
+                        self.best_position = position.reshape(1,self.options['n_vars'])
+                        self.best_fitness = bat_fitness
+                        self.best_bat = bat
+        
+
 
 
 if __name__=="__main__":
@@ -45,4 +111,14 @@ if __name__=="__main__":
     # print(w.shape, v.shape)
     # print(w)
 
-    print(initialize_population(10,5))
+    def cost(x):
+        return 20+np.e - 20*np.exp(-0.2*np.sqrt(np.sum(x*x)))-np.exp(np.sum(np.cos(2*np.pi*x)))
+
+    
+    optimizer = MDBA(n_vars=2,population_size=100,num_iterations=100,cost_func=cost,Ub=100,Lb=-100)
+    
+    optimizer.run()
+
+    print(optimizer.best_position)
+    # print(cost(np.zeros(shape=(2,2))[1,:]))
+
